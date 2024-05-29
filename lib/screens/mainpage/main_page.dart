9ceac79a70b19f3ccc334437c0cfe4fa164c8adc
zeppIine <kim0904min@gmail.com>
2024-05-29 20:35:18 +0900

    앱바에 입출차 토글버튼 생성

--- lib/screens/mainpage/main_page.dart
+++ lib/screens/mainpage/main_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pcg_pos/screens/mainpage/widget/app_bar.dart';
+import 'package:pcg_pos/widget/app_bar.dart';
 import 'package:pcg_pos/widget/MyElevatedBtn.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -14,28 +14,42 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _currentIndex = 0;
   late List<Widget> _pages;
+  bool _isEntry = true;
 
   @override
   void initState() {
     super.initState();
+    _pages = [
+      // 여기에 페이지 목록을 추가하세요.
+    ];
+  }
+
+  void _toggleEntryExit() {
+    setState(() {
+      _isEntry = !_isEntry;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(title: widget.title),
+      appBar: MyAppBar(
+        title: widget.title,
+        isEntry: _isEntry,
+        onToggle: _toggleEntryExit,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             SizedBox(
-              width: 300,
+              width: 400,
               height: 168,
               child: MyElevatedBtn(
                 onPressed: () {
                   Navigator.pushNamed(context, '/carnum');
                 },
-                text: '차량 입차',
+                text: _isEntry ? '차량 입차' : '차량 출차',
               ),
             )
           ],
