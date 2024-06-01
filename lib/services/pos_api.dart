5c490cec7d1d7d103356926b7f59c51e5436c68b
zeppIine <kim0904min@gmail.com>
2024-06-01 16:43:57 +0900

    jsonEncode 삭제

--- lib/services/pos_api.dart
+++ lib/services/pos_api.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:convert';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class ApiService {
@@ -12,13 +11,12 @@ class ApiService {
   static Future<bool> sendCarNum(String carNum, IO.Socket socket) async {
     final completer = Completer<Map<String, dynamic>>();
 
-    socket.emitWithAck('enter', jsonEncode({'carNum': carNum}), ack: (data) {
+    socket.emitWithAck('enter', {'carNum': carNum}, ack: (data) {
       completer.complete(data);
     });
 
     Map<String, dynamic> result = await completer.future;
 
-    print('$result');
     if (result.containsKey('paymentId')) {
       return true;
     } else {
