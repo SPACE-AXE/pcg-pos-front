7755f0e032ad3b8c1d3663323d8df7dc568b343e
zeppIine <kim0904min@gmail.com>
2024-06-02 21:08:57 +0900

    outParking, getUnpaid 구현

--- lib/services/pos_api.dart
+++ lib/services/pos_api.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:pcg_pos/car_data.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class ApiService {
@@ -24,7 +25,7 @@ class ApiService {
     }
   }
 
-  static Future<bool> tryExitCar(String carNum, IO.Socket socket) async {
+  static Future<bool> checkParking(String carNum, IO.Socket socket) async {
     final completer = Completer<Map<String, dynamic>>();
 
     socket.emitWithAck('exit', {'carNum': carNum}, ack: (data) {
@@ -37,6 +38,39 @@ class ApiService {
       return true;
     } else {
       return false;
+    }
+  }
+
+  static Future<bool> outParking(String carNum, IO.Socket socket) async {
+    final completer = Completer<Map<String, dynamic>>();
+
+    socket.emitWithAck('exit', {'carNum': carNum}, ack: (data) {
+      completer.complete(data);
+    });
+
+    Map<String, dynamic> result = await completer.future;
+    print(result);
+
+    if (result.containsKey('statusCode')) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  static Future<CarData> getUnpaid(String carNum, IO.Socket socket) async {
+    final completer = Completer<Map<String, dynamic>>();
+
+    socket.emitWithAck('getUnpaid', {'carNum': carNum}, ack: (data) {
+      completer.complete(data);
+    });
+
+    Map<String, dynamic> result = await completer.future;
+
+    if (result.containsKey('paymentId')) {
+      return CarData.fromJson(result);
+    } else {
+      throw Exception(result['statusCode']);
     }
   }
 }
